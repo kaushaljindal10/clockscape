ce983acffe7fd50c361c125323bb6b69b8510787
jindalmitin04 <jindalmitin04@gmail.com>
2026-09-20 04:48:25 +0530

    [CS001]: Updated the application id in build.gradle.kts (app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.ambientclock.wkqz"
+    applicationId = "com.devharbor.clockscape"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
